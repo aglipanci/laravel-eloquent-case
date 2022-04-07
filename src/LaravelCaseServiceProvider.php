33bcaff540c2c3f53d0114eba593afa5c081aa35
aglipanci <aglipanci@users.noreply.github.com>
2022-04-07 08:46:51 +0000

    Apply coding standard fixes

--- src/LaravelCaseServiceProvider.php
+++ src/LaravelCaseServiceProvider.php
@@ -35,7 +35,7 @@ class LaravelCaseServiceProvider extends ServiceProvider
 
         $this->app->bind(
             'casebuilder',
-            fn($app) => new CaseBuilder($app->make(Builder::class), new Grammar())
+            fn ($app) => new CaseBuilder($app->make(Builder::class), new Grammar())
         );
     }
 }
